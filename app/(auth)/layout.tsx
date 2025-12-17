97998b2a517bfa834f739780d68bdd34686d0111
Junyoung <compilevoid@gmail.com>
2025-12-17 15:38:48 +0900

    Create layout.tsx

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+export default function AuthLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <section>
+            auth
+            {children}
+        </section>
+    )
+}
